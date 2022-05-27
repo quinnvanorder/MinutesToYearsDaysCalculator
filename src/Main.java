dd9682015f36833d932023510d779cb31b1cfca9
Quinn Van Order <42633398+quinnvanorder@users.noreply.github.com>
2022-05-27 12:59:05 -0700

    Framework created and challenge detailed in main.

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/*
+Write a method printYearsAndDays with paremeter of type long named minutes.
+The method should not return anything (void) and it needs to calculate the years and days from the minutes parameter.
+If the parameter is less than 0 print "Invalid Value".
+Otherwise if the parameter is valid it needs to print a message in the format "XX min = YY y and ZZ d"
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
